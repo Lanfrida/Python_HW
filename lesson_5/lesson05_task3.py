a508d76871fe9f408c764c739c5ce1189fd295b5
Lanfrida <Lanfrida21@gmail.com>
2025-11-17 20:06:53 +0300

    lesson5

---
+++ lesson_5/lesson05_task3.py
@@ -0,0 +1,22 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get("http://the-internet.herokuapp.com/inputs")
+
+push_number = driver.find_element(By.CSS_SELECTOR, "input[type='number']")
+push_number.send_keys("Sky")
+
+sleep(3)
+
+push_number.clear()
+
+sleep(2)
+
+push_number.send_keys("Pro")
+
+sleep(3)
+
+driver.quit()
---
+++ lesson_5/lesson05_task4.py
@@ -0,0 +1,30 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get("http://the-internet.herokuapp.com/login")
+
+un = "input[name='username']"
+push_login = driver.find_element(By.CSS_SELECTOR, un)
+push_login.send_keys("tomsmith")
+
+sleep(1)
+
+pw = "input[id='password']"
+push_pass = driver.find_element(By.CSS_SELECTOR, pw)
+push_pass.send_keys("SuperSecretPassword!")
+
+sleep(2)
+
+log = "button.radius"
+push_log = driver.find_element(By.CSS_SELECTOR, log)
+push_log.click()
+
+success_message = driver.find_element(By.CSS_SELECTOR, "div.flash.success")
+print(success_message.text)
+
+sleep(6)
+
+driver.quit()
