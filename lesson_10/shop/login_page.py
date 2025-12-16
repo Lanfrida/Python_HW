cd56dc372e9a346fd71b14965f06844c9cb2ba85
sofiamaslikova <Lanfrida21@gmail.com>
2025-12-16 15:14:35 +0300

    lesson_10

---
+++ lesson_10/shop/login_page.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+from shop.base_page import BasePage
+from shop.product_page import ProductsPage
+
+
+class LoginPage(BasePage):
+    # Локаторы
+    USERNAME_INPUT = (By.ID, "user-name")
+    PASSWORD_INPUT = (By.ID, "password")
+    LOGIN_BUTTON = (By.ID, "login-button")
+    
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver.get("https://www.saucedemo.com")
+    
+    def login(self, username: str, password: str):
+        self.fill_field(*self.USERNAME_INPUT, username)
+        self.fill_field(*self.PASSWORD_INPUT, password)
+        self.click_element(*self.LOGIN_BUTTON)
+        return ProductsPage(self.driver)
